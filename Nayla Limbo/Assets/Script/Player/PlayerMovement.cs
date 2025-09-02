4ba02f79828b1fe4dc3283da7ad3eae8e98f7715
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2025-09-02 21:45:10 +0700

    Update: Movement + Project Settings

--- Nayla Limbo/Assets/Script/Player/PlayerMovement.cs
+++ Nayla Limbo/Assets/Script/Player/PlayerMovement.cs
@@ -2,44 +2,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float moveSpeed = 5f;
     public float deadZone = 0.01f;
+
+    [Header("References")]
     public Rigidbody2D rb;
     public Animator animator;
-    Vector2 movement;
+    public VirtualJoystick joystick; // assign kalau pakai joystick
 
-    // Simpan arah terakhir
+    Vector2 movement;
     float lastX;
     float lastY;
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        // ===== Input Handling =====
+        Vector2 inputVec;
 
+        if (joystick != null) // kalau ada joystick, pakai itu
+        {
+            inputVec = joystick.Direction; // sudah normalized (-1..1)
+        }
+        else // fallback: keyboard (Editor / PC)
+        {
+            inputVec.x = Input.GetAxisRaw("Horizontal");
+            inputVec.y = Input.GetAxisRaw("Vertical");
+        }
+
+        movement = inputVec;
+
+        // ===== Animator Parameters =====
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
+        // Simpan arah terakhir untuk Idle
         if (movement.sqrMagnitude > deadZone * deadZone)
         {
-            Vector2 lastDir = GetLastDirection(movement);
-            animator.SetFloat("LastX", lastDir.x);
-            animator.SetFloat("LastY", lastDir.y);
-        }
+            Vector2 last = (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+                ? new Vector2(Mathf.Sign(movement.x), 0f)
+                : new Vector2(0f, Mathf.Sign(movement.y));
 
+            lastX = last.x;
+            lastY = last.y;
+
+            animator.SetFloat("LastX", lastX);
+            animator.SetFloat("LastY", lastY);
+        }
     }
 
     void FixedUpdate()
     {
+        // ===== Physics Movement =====
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
-
-    Vector2 GetLastDirection(Vector2 move)
-    {
-        if (Mathf.Abs(move.x) > Mathf.Abs(move.y))
-            return new Vector2(Mathf.Sign(move.x), 0f);
-        else
-            return new Vector2(0f, Mathf.Sign(move.y));
     }
 }
