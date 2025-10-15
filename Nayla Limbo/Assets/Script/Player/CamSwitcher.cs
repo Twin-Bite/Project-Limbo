eeb470fdd879af52150b5dd7f0f774d8e9289ca2
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2025-10-15 23:07:34 +0700

    Update: CamSwitch

--- Nayla Limbo/Assets/Script/Player/CamSwitcher.cs
+++ Nayla Limbo/Assets/Script/Player/CamSwitcher.cs
@@ -3,38 +3,58 @@ using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(BoxCollider))]
 public class CamSwitcher : MonoBehaviour
 {
+    [Header("Auto Detect")]
     public Transform Player;
-
     public CinemachineCamera activeCam;
-    
+
+    [Header("Events")]
     public UnityEvent onEnterEvent;
     public UnityEvent onExitEvent;
 
+    private void Reset()
+    {
+        BoxCollider col = GetComponent<BoxCollider>();
+        col.isTrigger = true;
+    }
 
+    private void Awake()
+    {
+        if (Player == null)
+        {
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+            if (playerObj != null) Player = playerObj.transform;
+        }
 
-  
+        if (activeCam == null)
+        {
+            activeCam = GetComponentInChildren<CinemachineCamera>();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            activeCam.Priority = 1;
+            if (activeCam != null)
+            {
+                activeCam.Priority = 10;
+            }
             onEnterEvent.Invoke();
-            
         }
-        
-      
     }
-    
-    
+
     private void OnTriggerExit(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            activeCam.Priority = 0;
+            if (activeCam != null)
+            {
+                activeCam.Priority = 0;
+            }
             onExitEvent.Invoke();
-           
         }
     }
 }
