69a5f5633db0801713a412bb9c623651cc570c9c
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2026-05-09 21:53:13 +0700

    Fix: Sanity

--- Nayla Limbo/Assets/Script/Player/Sanity/Sanity.cs
+++ Nayla Limbo/Assets/Script/Player/Sanity/Sanity.cs
@@ -1,16 +1,80 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Sanity : MonoBehaviour
+public class SanitySystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public float maxSanity = 100f;
+   public float currentSanity;
+   public float drainRate = 5f;
+   public float rechargeRate = 10f;
+   public float delayBeforeDrain = 15f;
+   public Slider sanitySlider;
+
+   private Coroutine sanityRoutine;
+   private bool isDraining = false;
+   private bool isInZone = false;
+
+   void Start()
     {
-        
+        currentSanity = maxSanity;
+        UpdateUI();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isInZone)
+        {
+            currentSanity = Mathf.MoveTowards(currentSanity, maxSanity, rechargeRate * Time.deltaTime);
+        } else if (isDraining)
+        {
+            currentSanity = Mathf.MoveTowards(currentSanity, 0f, drainRate * Time.deltaTime);
+        }
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (sanitySlider)
+        {
+            sanitySlider.maxValue = maxSanity;
+            sanitySlider.value = currentSanity;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SanityZone"))
+            StartDrainSequence();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("SanityZone") && !isInZone)
+            StartDrainSequence();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("SanityZone"))
+        {
+            isInZone = false;
+            if (sanityRoutine != null) StopCoroutine(sanityRoutine);
+            isDraining = false;
+        }
+    }
+
+    private void StartDrainSequence()
+    {
+        isInZone = true;
+        if (sanityRoutine != null) StopCoroutine(sanityRoutine);
+        sanityRoutine = StartCoroutine(WaitAndDrain());
+    }
+
+    private IEnumerator WaitAndDrain()
+    {
+        yield return new WaitForSeconds(delayBeforeDrain);
+        if (isInZone) isDraining = true;
     }
 }
